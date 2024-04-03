46a6615a13d77e162c02cab7b07ca670c1eb6211
Yoga Hapriana <thenelse.v2@gmail.com>
2024-04-04 06:39:52 +0700

    Deleting Chirps livewire

--- chirper-livewire/resources/views/livewire/chirps/list.blade.php
+++ chirper-livewire/resources/views/livewire/chirps/list.blade.php
@@ -38,6 +38,15 @@ new class extends Component {
  
         $this->getChirps();
     } 
+
+    public function delete(Chirp $chirp): void
+    {
+        $this->authorize('delete', $chirp);
+ 
+        $chirp->delete();
+ 
+        $this->getChirps();
+    } 
 }; ?>
 
 <div class="mt-6 bg-white shadow-sm rounded-lg divide-y">
@@ -68,6 +77,9 @@ new class extends Component {
                                 <x-dropdown-link wire:click="edit({{ $chirp->id }})">
                                     {{ __('Edit') }}
                                 </x-dropdown-link>
+                                <x-dropdown-link wire:click="delete({{ $chirp->id }})" wire:confirm="Are you sure to delete this chirp?"> 
+                                    {{ __('Delete') }}
+                                </x-dropdown-link> 
                             </x-slot>
                         </x-dropdown>
                     @endif
