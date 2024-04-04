4619d3fa1aac5c8612582de9433e1debd167ec28
Yoga Hapriana <thenelse.v2@gmail.com>
2024-04-04 16:30:36 +0700

    Editing Chirps Inertia

--- chirper-inertia/app/Http/Controllers/ChirpController.php
+++ chirper-inertia/app/Http/Controllers/ChirpController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Gate;
 
 class ChirpController extends Controller
 {
@@ -61,9 +62,17 @@ class ChirpController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Chirp $chirp)
+    public function update(Request $request, Chirp $chirp): RedirectResponse
     {
-        //
+        Gate::authorize('update', $chirp);
+ 
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+ 
+        $chirp->update($validated);
+ 
+        return redirect(route('chirps.index'));
     }
 
     /**
